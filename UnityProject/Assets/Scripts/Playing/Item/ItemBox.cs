11f581ba29e8f9e9c9d85f114f84c23304227bef
WonSang98 <cdsywon@kookmin.ac.kr>
2023-03-20 17:43:21 +0900

    feature : Add ItemBox & Backup the Testmap00

---
+++ UnityProject/Assets/Scripts/Playing/Item/ItemBox.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour
+{
+    private void Start()
+    {
+        gameObject.layer = 0; //Default Layer;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Kart") && gameObject.layer != 30)
+        {
+            StartCoroutine(RemakeBox(other.transform.GetComponent<KartController>()));
+        }
+    }
+
+    ITEMS SelectItem()
+    {
+        //아이템을 획득한 유저의 상황에 맞추어 아이템을 설정함.
+        //현재는 부스터밖에 없으므로 부스터만.
+        return ITEMS.BOOST;
+    }
+
+    IEnumerator RemakeBox(KartController user)
+    {
+        //아이템 획득 시, 박스 disable
+        //n초 후 재생성
+        user.hasItem = SelectItem();
+        gameObject.layer = 30;
+        yield return new WaitForSeconds(2f);
+        gameObject.layer = 0;
+    }
+}
