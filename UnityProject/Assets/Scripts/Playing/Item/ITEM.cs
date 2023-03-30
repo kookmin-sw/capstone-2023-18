cae474a23c75f58c337ba0704818e3a4c41a63e9
WonSang98 <cdsywon@kookmin.ac.kr>
2023-03-30 19:43:02 +0900

    fix : Item Script Fix and Update to Kart Box Collider

---
+++ UnityProject/Assets/Scripts/Playing/Item/ITEM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public enum ITEMS
+{
+    NONE = 0,
+    BOOST = 1
+}
+public class ITEM : MonoBehaviour
+{
+    [Header("ICON IMAGES")]
+    public Sprite[] ICONS;
+}
