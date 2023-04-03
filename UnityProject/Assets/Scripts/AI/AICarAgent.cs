b3e54bd108e04c309e7e70f17356b3db2c6c56a7
archun39 <archun39@kookmin.ac.kr>
2023-04-03 10:58:22 +0900

    Add sac file

--- UnityProject/Assets/Scripts/AI/AICarAgent.cs
+++ UnityProject/Assets/Scripts/AI/AICarAgent.cs
@@ -9,36 +9,33 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-
-
 public class AICarAgent : Agent
 {
     
     [Header("===Input===")]
-    public float Hmove;
-    public float Vmove;
-    public bool Drift;
-    public bool Item;
     private KartInput input;
+    public GameObject cp; // red line
     
 
     [Header("===Info===")] 
-    
-    [SerializeField] private Transform checkpoint;
-    [SerializeField] private GameObject startPos;
+    [SerializeField] private Transform checkpoint; // front of red line
 
-    private Transform startLine;
+
+    private GameObject startPos;
+    public Transform startLine;
+
     private Transform agentTransform;
     private Rigidbody agentRigidbody;
+
+
+    //For check spinning
+    private Vector3 nowPos; 
     private float driftTime;
-
-    private Vector3 nowPos; // For check spin
-
 
 
     public override void Initialize()
     {
-        startLine = GameObject.Find("startLine").transform;
+        startLine = GameObject.Find("startPos").transform;
         
         input = GetComponent<KartInput>();
         agentTransform = GetComponent<Transform>();
@@ -46,19 +43,21 @@ public class AICarAgent : Agent
        
         checkpoint = startLine;
 
-        Hmove = input.Hmove;
-        Vmove = input.Vmove;
-
     }
     public override void OnEpisodeBegin()
     {
-
         driftTime = 0f;
-        nowPos = agentTransform.localPosition;
+        nowPos = agentTransform.position;
+        cp = startPos;
+
+
+
         //위치정보 초기화
         agentRigidbody.velocity = Vector3.zero;
-        agentTransform.localRotation = Quaternion.Euler(0,0,0);
+        agentRigidbody.angularVelocity = Vector3.zero;
+        SetResetPos(startLine);
         GoResetPos();
+        
     }
     
     public override void CollectObservations(VectorSensor sensor)
@@ -68,54 +67,58 @@ public class AICarAgent : Agent
         sensor.AddObservation(agentTransform.rotation); //3
         sensor.AddObservation(transform.InverseTransformDirection(agentRigidbody.velocity).magnitude); //1 local velocity of car
         
-        
     }
     
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         input.Vmove = 1f;
+        input.Drift = actionBuffers.ContinuousActions[1] > 0 ? true : false;
         input.Hmove = actionBuffers.ContinuousActions[0];
-        input.Drift = actionBuffers.DiscreteActions[0] == 1 ? true : false;
-        //input.Item = actionBuffers.DiscreteActions[1] == 1 ? true : false;
-        
-        AddReward(2f/MaxStep);
+
+        AddReward(3f/MaxStep);
     }
     
     public override void Heuristic(in ActionBuffers actionOut)
     {
+        
         ActionSegment<float> continuousActions = actionOut.ContinuousActions;
-        ActionSegment<int> discreteActions = actionOut.DiscreteActions;
+
+        continuousActions[1]= Input.GetKey(KeyCode.LeftShift) ? 1f : 0f;
         continuousActions[0] = Input.GetAxis("Horizontal");
-        discreteActions[0] = Input.GetKey(KeyCode.LeftShift) ? 1 : 0;
-        //discreteActions[1] = Input.GetKey(KeyCode.Z) ? 1 : 0;
+ 
+        
+           
     }
     
+
     public void FixedUpdate(){
         
         driftTime += Time.fixedDeltaTime;
 
-        if(driftTime > 1f){
-            //Debug.Log(Mathf.Abs((nowPos-agentTransform.localPosition).magnitude));
-            if(Mathf.Abs((nowPos-agentTransform.localPosition).magnitude) < 10f){
-                //Debug.Log("spin");
-                SetReward(-2f);
-                EndEpisode();
+        //check spin or don't move
+        if(driftTime > 2f){
+            float dis = Mathf.Abs((nowPos-agentTransform.localPosition).magnitude);
+            
+            if(dis < 10f){
+                AddReward(-1f);
+                GoResetPos();
             }
             driftTime = 0f;
             nowPos = agentTransform.localPosition;
         }
     }
 
+    //set checkpoint
     public void SetResetPos(Transform tmp){
         checkpoint = tmp;
     }
 
-    private void GoResetPos(){
-        
-        Debug.Log(checkpoint.rotation);
-        agentTransform.position = checkpoint.position + new Vector3(0.5f,1.5f,0.5f);
-       
 
-
+    //move agent to cp
+    public void GoResetPos(){
+        agentRigidbody.velocity = Vector3.zero;
+        agentRigidbody.angularVelocity = Vector3.zero;
+        agentTransform.position = checkpoint.position + new Vector3(0f,0.3f,0f);
+        agentTransform.rotation = checkpoint.rotation;
     }
 }
--- UnityProject/Assets/Scripts/AI/Sensor.cs
+++ UnityProject/Assets/Scripts/AI/Sensor.cs
@@ -9,39 +9,54 @@ public class Sensor : MonoBehaviour
     private AICarAgent script;
     public float nowTime;
     
-    public void Start()
+
+    public void Awake()
     {
         script = GetComponentInParent<AICarAgent>();
     }
     
-    public void OnTriggerStay (Collider collider)
-    {
-        if (collider.gameObject.CompareTag("Wall"))
-        {
-            script.AddReward(-1f);
-            script.EndEpisode();
-
-        }
-    }
 
     public void OnTriggerEnter(Collider collider){
+        
         if(collider.gameObject.CompareTag("Goal")){
             script.AddReward(1f);
             script.EndEpisode();
         }
 
         if(collider.gameObject.CompareTag("Guide")){
-            script.AddReward(-2f);
+            script.AddReward(-1f);
             script.EndEpisode();
         }
 
         if(collider.gameObject.CompareTag("Checkpoint")){
-            Transform tmp = collider.gameObject.transform;
-            script.SetResetPos(tmp);
+            
+            if(script.cp == collider.gameObject){
+                script.AddReward(-1f);
+                script.EndEpisode();
+            }
+            else{
+                
+                Transform tmp = collider.gameObject.transform;
+                tmp = tmp.GetChild(0).gameObject.transform;
+                script.cp = collider.gameObject;
+                script.AddReward(0.5f);
+                script.SetResetPos(tmp);
+                
+            }
+        }
+    }
+    public void OnTriggerStay(Collider collider){
+        if(collider.gameObject.CompareTag("Way")){
+            script.AddReward(2f/script.MaxStep);
+
+        }
+        if(collider.gameObject.CompareTag("Wall")){
+            script.AddReward(-1f);
+            script.EndEpisode();
         }
     }
     
-    
+   
     
     
 }
