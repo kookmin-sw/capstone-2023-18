7e1a87d76c48008b4541530d93998db9044a8784
WonSang98 <cdsywon@kookmin.ac.kr>
2023-05-22 13:08:52 +0900

    featrue : AI 대결 업데이트

--- UnityProject/Assets/Scripts/Network/Play/NetPlayerInfo.cs
+++ UnityProject/Assets/Scripts/Network/Play/NetPlayerInfo.cs
@@ -213,6 +213,7 @@ public class NetPlayerInfo : NetworkBehaviour, IComparable<NetPlayerInfo>
             }
 
         }
+
     }
 
 
---
+++ UnityProject/Assets/Scripts/Network/Play/SingleEndGame.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleEndGame : MonoBehaviour
+{
+    public NetPlayManager npm;
+    public bool isEnd;
+
+
+    public GameObject LoseUI;
+    public GameObject WinUI;
+    private void Start()
+    {
+        isEnd = false;
+        LoseUI.SetActive(false);
+        WinUI.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (isEnd == false)
+        {
+            Debug.Log(other.name);
+
+            if (other.CompareTag("AI"))
+            {
+                Debug.Log("yes");
+                isEnd = true;
+                LoseUI.SetActive(true);
+            }
+
+            if (other.CompareTag("Kart"))
+            {
+                isEnd = true;
+                WinUI.SetActive(true);
+            }
+        }
+    }
+}
